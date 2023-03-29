5df4029fa322a4432d8f1025cd0848db45d38229
abilimisi <123450862+abilimisi@users.noreply.github.com>
2023-03-29 17:13:55 +0530

    Add files via upload

---
+++ contact/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render,redirect
+
+
+# Create your views here.
+def home(request):
+    return render(request,'index.html')
+
+def signup(request):
+
+    return render(request,'signup.html')
+
+def signin(request):
+    return render(request,'signin.html')
+
+def signout(request):
+    return render(request,'signout.html')
